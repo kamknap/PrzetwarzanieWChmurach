9364ff426461423bf534286aca0d52fb75c3bf77
Kamil Knapik <knapik.kamil@outlook.com>
2025-12-04 22:04:00 +0100

    Update conftest.py

--- e2e-tests/conftest.py
+++ e2e-tests/conftest.py
@@ -1,9 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import pytest
+import os
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    
+    # Tryb headless dla serwerów bez GUI (np. Jenkins na Linuxie)
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1920,1080')
+    
+    # Ukrycie informacji o automatyzacji
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
